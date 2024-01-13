2bd3cff69533e527b4d9316a3d57e50b5d58a9bb
Vedant Chainani <vedantchainani1084@gmail.com>
2024-01-13 19:53:10 +0530

    add github and twitter aa wallets

--- src/providers/web3/index.tsx
+++ src/providers/web3/index.tsx
@@ -1,20 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 'use client';
 
 import { WagmiConfig, createConfig } from 'wagmi';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import { supportedSocialConnectors } from '@zerodev/wagmi/connectkit';
+import {
+	supportedConnectors,
+	getDefaultConfig,
+	ConnectKitProvider,
+	getDefaultConnectors,
+} from 'connectkit';
+supportedConnectors.push(...supportedSocialConnectors);
+
 import { sepolia } from 'wagmi/chains';
 
+import {
+	GithubSocialWalletConnector,
+	TwitterSocialWalletConnector,
+} from '@zerodev/wagmi';
+
 import { env } from '~/env';
+
+const chains = [sepolia];
+
+const defaultConnectors = [
+	...getDefaultConnectors({
+		chains,
+		app: { name: 'GHO Tunes' },
+		walletConnectProjectId: env.NEXT_PUBLIC_WALLETCONNECT_ID,
+	}),
+];
+
+const socialConnectors = defaultConnectors.slice(0, 1);
+const otherConnectors = defaultConnectors.slice(2);
+
+const options = {
+	chains: [sepolia],
+	options: { projectId: env.NEXT_PUBLIC_WALLETCONNECT_ID },
+};
 
 const config = createConfig(
 	getDefaultConfig({
 		alchemyId: env.NEXT_PUBLIC_ALCHEMY_KEY,
 		walletConnectProjectId: env.NEXT_PUBLIC_WALLETCONNECT_ID,
-		chains: [sepolia],
-		appName: 'GHOTUNES',
-		appDescription: 'Your App Description',
-		appUrl: 'https://family.co',
-		appIcon: 'https://family.co/logo.png',
+		appName: 'GHO Tunes',
+		chains,
+		connectors: [
+			...socialConnectors,
+			new GithubSocialWalletConnector(options),
+			new TwitterSocialWalletConnector(options),
+			...otherConnectors,
+		],
 	})
 );
 
